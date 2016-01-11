025186dc0c8b43b33a5f5d7e684e6ab0af734fc2
Nadine Kinnen <nadine@cotap.com>
2016-01-11 12:40:14 -0800

    Cleanup

--- app/application.js
+++ app/application.js
@@ -2,121 +2,118 @@
 
 var App = {
   init: function init() {
-    var gameModule = angular.module('ticTacToeApp', []);
+    angular.module('ticTacToeApp', []);
 
-    gameModule.controller('gameController', ['$scope','GameDataService', function($scope, GameDataService) {
+    angular.module('ticTacToeApp').controller('gameController',
+                                  ['$scope','GameDataService', function($scope, GameDataService) {
       var player1 = 'X';
       var player2 = 'O';
       var played = null;
-
       $scope.currentPlayer = player1;
       $scope.newSize = 3;
       $scope.newNumberToWin = 3;
 
       var initData = function() {
+        played = [];
         $scope.winner = null;
         $scope.draw = null;
-        played = [];
         $scope.boardsize =  $scope.newSize;
         $scope.numberToWin = $scope.newNumberToWin;
-        $scope.data = GameDataService.getBoardBySize($scope.boardsize);
+        $scope.board = GameDataService.getBoardBySize($scope.boardsize);
       };
 
-      var evaluateTurn = function(item) {
-        played.push(item);
-
-        var movesByPlayer = _.filter(played,{'move':$scope.currentPlayer});
+      var evaluateTurn = function() {
+        var movesByPlayer = _.filter(played, { 'player': $scope.currentPlayer });
         if (movesByPlayer.length < $scope.numberToWin) {
           return; //not enough moves, don't bother checking
         }
 
-        if (movesByPlayer.length >= $scope.numberToWin) {
-          var slideDownCount = 0; //00, 11, 22 //diagonal top left to bottom right
-          var slideUpCount = 0;  //20, 11, 02 //diagonal bottom left to top right
-          for(var i = 0; i < $scope.numberToWin; i++) {
-            var rowCount = 0;
-            var colCount = 0;
-            var countDown = $scope.numberToWin - 1 - i;
+        var diagonalDownCount = 0; //00, 11, 22 - diagonal top left to bottom right positions
+        var diagonalUpCount = 0;  //20, 11, 02 - diagonal bottom left to top right positions
 
-            _.each(movesByPlayer, function(move) {
-              if (move.row === i && move.col === i) {
-                slideDownCount += 1;
-              }
+        for (var i = 0; i < $scope.numberToWin; i++) {
+          var rowCount = 0; //check every row
+          var colCount = 0;  //check every column
+          var diagonalUpIndex = $scope.numberToWin - 1 - i; //2, 1, 0 diagonal up column index
 
-              if (move.row === countDown && move.col === i) {
-                slideUpCount += 1;
-              }
+          _.each(movesByPlayer, function(move) {
+            if (move.row === i && move.col === i) {
+              diagonalDownCount += 1;
+            }
+            if (move.row === diagonalUpIndex && move.col === i) {
+              diagonalUpCount += 1;
+            }
+            if (move.row === i) {
+              rowCount += 1;
+            }
+            if (move.col === i) {
+              colCount += 1;
+            }
+            if (diagonalUpCount === $scope.numberToWin
+                || diagonalDownCount === $scope.numberToWin
+                || colCount === $scope.numberToWin
+                || rowCount === $scope.numberToWin) {
 
-              if (move.row === i) {
-                rowCount += 1;
-              }
+              $scope.winner = $scope.currentPlayer;
+              return;
+            }
+          });
+       }
 
-              if (move.col === i) {
-                colCount += 1;
-              }
-
-              if (slideUpCount === $scope.numberToWin
-                  || slideDownCount === $scope.numberToWin
-                  || colCount === $scope.numberToWin
-                  || rowCount === $scope.numberToWin) {
-                $scope.winner = $scope.currentPlayer;
-                return;
-              }
-            });
-          }
-        }
-
-        if (played.length >= ($scope.numberToWin * $scope.numberToWin) && !$scope.winner) {
+       //no winner but all positions have been played
+        if (played.length === ($scope.boardsize * $scope.boardsize)) {
           $scope.draw = true;
-          return;
         }
       };
 
       var changePlayer = function() {
-        if ($scope.currentPlayer === player1) {
-          $scope.currentPlayer = player2;
-        } else {
-          $scope.currentPlayer = player1;
-        }
+        $scope.currentPlayer = ($scope.currentPlayer === player1)
+          ? player2
+          : player1;
       };
 
-      this.playTurn = function($event, item, player) {
+      this.playTurn = function($event, positionInfo) {
         $scope.errors = null;
 
-        if (!$scope.winner) {
-          if (item.move !== null) {
-            //$scope.errors = 'This square has already been played';
-            return;
-          }
+        if ($scope.winner) {
+          return;
+        }
 
-          item.move = $scope.currentPlayer;
-          evaluateTurn(item);
+        if (positionInfo.player !== null) {
+          return;
+        }
 
-          if (!$scope.winner) { //change players
-            changePlayer();
-          }
+        positionInfo.player = $scope.currentPlayer;
+        played.push(positionInfo);
+        evaluateTurn();
+
+        if (!$scope.winner) { //change players if no winner
+          changePlayer();
         }
       };
 
       this.newGame = function($event) {
           if ($scope.newNumberToWin > $scope.newSize) {
               $scope.errors = "Please choose a winning number smaller or equal to the board size.";
-          }else{
+          } else {
             $scope.errors = null;
             initData();
           }
       };
 
       initData();
+
     }]);
 
-    gameModule.factory('GameDataService', function() {
+
+    angular.module('ticTacToeApp').factory('GameDataService', function() {
       var service = {};
+
       var createBoard = function(boardsize) {
         var data = [];
-          for(var i = 0; i<boardsize; i++) {
-            for(var j = 0; j<boardsize; j++) {
-              data.push({'row':i, 'col': j, 'move':null});
+          for (var i = 0; i < boardsize; i++) {
+            for (var j = 0; j < boardsize; j++) {
+              data.push({'row':i, 'col': j, 'player': null});
             }
           }
         return data;
@@ -131,8 +128,7 @@ var App = {
       };
 
       return service;
-    }); //end service
-
+    });
   }
 };
 
